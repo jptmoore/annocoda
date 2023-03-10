286685767e44b9e857a4b691220afd7254ae6680
John Moore <john.moore@nationalarchives.gov.uk>
2023-03-10 18:16:14 +0000

    use bootstrap text input

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
-from dash import Dash, dash_table
+from dash import Dash, html, dcc, dash_table
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
 from data import Data
 import logging
 
@@ -17,12 +19,36 @@ app = Dash(__name__)
 
 ctx.logger = app.logger
 data = Data(ctx)
-result = data.get_annotation(url="https://miiify.rocks/iiif/content/search?q=henry")
-app.layout = dash_table.DataTable(
-    data=result,
-    style_cell={"textAlign": "left"},
-    style_data={"whiteSpace": "normal", "height": "auto", "lineHeight": "15px"},
+result = []
+
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Annocoder"),
+        html.Div(children="Search"),
+        dbc.Input(id="keywords", placeholder="keywords...", type="text"),
+        html.Button("Submit", id="search-button", n_clicks=0),
+        dash_table.DataTable(
+            id="annotations",
+            data=result,
+            style_cell={"textAlign": "left"},
+            style_data={"whiteSpace": "normal", "height": "auto", "lineHeight": "15px"},
+        ),
+    ]
 )
+
+
+@app.callback(
+    Output("annotations", "data"),
+    Input("search-button", "n_clicks"),
+    State("keywords", "value"),
+)
+def update_output(n_clicks, value):
+    if n_clicks > 0:
+        result = data.get_annotation(
+            url=f"https://miiify.rocks/iiif/content/search?q={value}"
+        )
+        return result
 
 
 if __name__ == "__main__":
