40477a6fc506150f2bfa8a7c319645deeabbf86c
John Moore <jmoore@zedstar.org>
2023-03-17 16:01:16 +0000

    change demo data

--- src/components/carousel.py
+++ src/components/carousel.py
@@ -8,18 +8,23 @@ carousel = (
         indicators=True,
         items=[
             {
-                "key": "foo",
+                "key": "logo",
+                "src": "../assets/annocoda-high-resolution-logo-color-on-transparent-background.png",
+                "img_style": {"height": "10%", "width": "10%"},
+            },
+            {
+                "key": "1",
                 "src": "https://upload.wikimedia.org/wikipedia/commons/c/c2/Fat_cat%2C_asleep_%28319313958%29.jpg",
                 "img_style": {"height": "10%", "width": "10%"},
             },
             {
-                "key": "bar",
+                "key": "2",
                 "src": "https://upload.wikimedia.org/wikipedia/commons/9/90/Crimean_Tom.jpg",
                 "img_style": {"height": "10%", "width": "10%"},
             },
             {
-                "key": "baz",
-                "src": "https://upload.wikimedia.org/wikipedia/commons/1/16/Stationmaster_NITAMA_20110105.jpg",
+                "key": "19",
+                "src": "https://upload.wikimedia.org/wikipedia/commons/1/18/Socks_the_Cat_Explores.jpg",
                 "img_style": {"height": "10%", "width": "10%"},
             },
         ],
