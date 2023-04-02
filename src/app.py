467c2f04d92640cf548c95bf6596d31688eda6c8
John Moore <jmoore@zedstar.org>
2023-04-02 22:44:04 +0100

    duplicate output

--- src/app.py
+++ src/app.py
@@ -70,9 +70,11 @@ def deselectRows(selected_cells):
 @app.callback(
     Output("carousel", "active_index"),
     Output("table", "active_cell"),
+    Output("carousel", "items", allow_duplicate=True),
     Input("carousel", "items"),
     Input("table", "active_cell"),
     State("table", "data"),
+    prevent_initial_call=True
 )
 def getActiveCell(items, active_cell, data):
     if active_cell:
@@ -82,9 +84,9 @@ def getActiveCell(items, active_cell, data):
         index = manifest.index_of_target(target)
         src = items[index].get("src")
         print("box:", box, "src:", src)
-        return index, None
+        return index, None, items
     else:
-        return 0, active_cell
+        return 0, active_cell, items
 
 
 @app.callback(
