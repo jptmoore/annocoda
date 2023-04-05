a2e8a5b6eeb7a8bab7227543445ae7497d302a5d
John Moore <jmoore@zedstar.org>
2023-04-05 22:52:03 +0100

    drawing incorrect box

--- src/polygon.py
+++ src/polygon.py
@@ -1,6 +1,7 @@
 import cv2
 import requests
 from PIL import Image
+import numpy as np
 
 class Polygon:
     def __init__(self, ctx):
@@ -12,19 +13,21 @@ class Polygon:
         return image
 
     def draw_bounding_box_worker(self, image, xywh):
+        data = np.asarray(image)
         color = (255, 0, 0)
         left,top,right,bottom = xywh
         label = 'annotated'
-        imgHeight, imgWidth, _ = image.shape
+        imgHeight, imgWidth, _ = data.shape
         thick = int((imgHeight + imgWidth) // 900)
-        cv2.rectangle(image,(left, top), (right, bottom), color, thick)
-        cv2.putText(image, label, (left, top - 12), 0, 1e-3 * imgHeight, color, thick//3)
-        return image
+        cv2.rectangle(data,(left, top), (right, bottom), color, thick)
+        cv2.putText(data, label, (left, top - 12), 0, 1e-3 * imgHeight, color, thick//3)
+        return Image.fromarray(data)
 
 
     def draw_bounding_box(self, url, xywh):
         image = self.load_image(url)
+        bounded_image = self.draw_bounding_box_worker(image, xywh)
         return {
              "key": "annotated",
-             "src": image
+             "src": bounded_image
         }
