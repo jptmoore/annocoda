6112514096ceae5b71407db7c49404d2bd6c2cbf
John Moore <john.moore@nationalarchives.gov.uk>
2023-06-13 15:16:10 +0100

    start to remove redundant data

--- src/manifest.py
+++ src/manifest.py
@@ -1,4 +1,3 @@
-import requests
 from jsonpath_ng import parse
 import dash_bootstrap_components as dbc
 
@@ -24,9 +23,6 @@ class Manifest:
             return []
         else:
             return result
-
-    def get_image_count(self, json):
-        return len(self.__get_image_links__(json))
 
     def get_targets(self, json):
         try:
@@ -75,57 +71,6 @@ class Manifest:
             return self.nothing_found()
         else:
             return result
-
-    def make_target_list(self):
-        targets = self.data.keys()
-        return targets
-
-    def get_frag_selector_cords_worker(self, frag):
-        res = frag.split(',')
-        match res: 
-            case [_, x, y, w, h]:
-                return (x,y,w,h)
-            case _:
-                raise ValueError("failed to match cords")
-
-    def get_frag_selector_cords(self, target):
-        res = target.split('#xywh=')
-        match res: 
-            case [_, y]:
-                result = tuple(map(int, y.split(',')))
-                return result
-            case [_]:
-                return None
-            case _:
-                raise ValueError("failed to match target")
-
-
-    def remove_frag_selector(self, target):
-        res = target.split("#")
-        match res: 
-            case [x, _]:
-                return x
-            case [x]:
-                return x
-            case _:
-                raise ValueError("failed to match target")
-
-    def index_of_target(self, target):
-        k = self.remove_frag_selector(target)
-        try:
-            result = self.current_targets.index(k)
-        except ValueError as e:
-            self.logger.error(f"failed to find target: {repr(e)}")
-            return None        
-        return result
-
-
-    def filter_result_data(self, annotation_targets):
-        data = self.data
-        filtered_data = dict((k, data[k]) for k in annotation_targets if k in data)
-        self.current_targets = list(filtered_data.keys())
-        result = self.make_result_data(filtered_data)
-        return result
 
     def manifest_result(self, json):
         try:
