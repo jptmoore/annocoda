42efd0be4a47d7d888a9b536341814ca46e0f97e
John Moore <jmoore@zedstar.org>
2023-07-24 17:37:31 +0100

    improve tray button

--- annocoda/components/statusbar.py
+++ annocoda/components/statusbar.py
@@ -2,6 +2,6 @@ import dash_bootstrap_components as dbc
 from dash import html
 
 
-statusbar = html.H3(
-    dbc.Badge(color="primary", id="status-bar", n_clicks=0, className="d-grid gap-2 col-6 mx-auto")
+statusbar = html.Div(
+    dbc.Button(color="primary", id="status-bar", outline=True, n_clicks=0, className="d-grid gap-2 col-6 mx-auto")
 )
