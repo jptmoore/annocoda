51bff7c24fa942f0b85b007fd90bd4d99cd4cac1
John Moore <jmoore@zedstar.org>
2023-08-30 17:37:02 +0100

    improvement to splash screen

--- annocoda/components/splash.py
+++ annocoda/components/splash.py
@@ -1,5 +1,5 @@
 import dash_bootstrap_components as dbc
-from dash import html
+from dash import html, DiskcacheManager
 
 splash = html.Div(
     dbc.Container(
@@ -12,25 +12,88 @@ splash = html.Div(
             dbc.ListGroup(
                 [
                     dbc.ListGroupItem(
-                        "Women war workers on the Metropolitan Railway",
-                        href="?manifest=https://miiify.rocks/manifest/diamond_jubilee_of_the_metro&search=women%20war",
+                        [
+                            html.Div(
+                                [
+                                    html.H5(
+                                        "Women war workers on the Metropolitan Railway",
+                                        className="mb-1",
+                                    ),
+                                    html.Small("\U0001F680", className="text-success"),
+                                ],
+                                className="d-flex w-100 justify-content-between",
+                            ),
+                            html.A(
+                                "https://annocoda.onrender.com/?manifest=https://miiify.rocks/manifest/diamond_jubilee_of_the_metro&search=women%20war",
+                                href="?manifest=https://miiify.rocks/manifest/diamond_jubilee_of_the_metro&search=women%20war",
+                            ),
+                        ]
                     ),
                     dbc.ListGroupItem(
-                        "Mentions of Pinner in Rustic walking routes from within the twelve-mile radius of Charing Cross",
-                        href="?manifest=https://miiify.rocks/manifest/rustic_walking_routes&search=pinner",
+                        [
+                            html.Div(
+                                [
+                                    html.H5(
+                                        "Mentions of Pinner in Rustic walking routes from within the twelve-mile radius of Charing Cross",
+                                        className="mb-1",
+                                    ),
+                                    html.Small("\U0001F680", className="text-success"),
+                                ],
+                                className="d-flex w-100 justify-content-between",
+                            ),
+                            html.A(
+                                "https://annocoda.onrender.com/?manifest=https://miiify.rocks/manifest/rustic_walking_routes&search=pinner",
+                                href="?manifest=https://miiify.rocks/manifest/rustic_walking_routes&search=pinner",
+                            ),
+                        ]
                     ),
                     dbc.ListGroupItem(
-                        "Monasteries within an Introduction to the Valor Ecclesiasticus",
-                        href="?manifest=https://miiify.rocks/manifest/intro_valor_ecclesiasticus&search=monasteries",
+                        [
+                            html.Div(
+                                [
+                                    html.H5(
+                                        "Monasteries within an Introduction to the Valor Ecclesiasticus",
+                                        className="mb-1",
+                                    ),
+                                    html.Small("\U0001F680", className="text-success"),
+                                ],
+                                className="d-flex w-100 justify-content-between",
+                            ),
+                            html.A(
+                                "https://annocoda.onrender.com/?manifest=https://miiify.rocks/manifest/intro_valor_ecclesiasticus&search=monasteries",
+                                href="?manifest=https://miiify.rocks/manifest/intro_valor_ecclesiasticus&search=monasteries",
+                            ),
+                        ]
                     ),
                     dbc.ListGroupItem(
-                        "Greece in The works of the British poets",
-                        href="?manifest=https://miiify.rocks/manifest/british_poets_1759_1834&search=greece",
-                    ),                             
+                        [
+                            html.Div(
+                                [
+                                    html.H5(
+                                        "Greece in the works of the British poets",
+                                        className="mb-1",
+                                    ),
+                                    html.Small("\U0001F680", className="text-success"),
+                                ],
+                                className="d-flex w-100 justify-content-between",
+                            ),
+                            html.A(
+                                "https://annocoda.onrender.com/?manifest=https://miiify.rocks/manifest/british_poets_1759_1834&search=greece",
+                                href="?manifest=https://miiify.rocks/manifest/british_poets_1759_1834&search=greece",
+                            ),
+                        ]
+                    ),
                 ]
             ),
             html.P(),
-            html.P(dbc.Button("Learn more", color="primary"), className="lead"),
+            html.P(
+                dbc.Button(
+                    "Learn more",
+                    color="primary",
+                    href="https://github.com/jptmoore/annocoda",
+                ),
+                className="lead",
+            ),
         ],
         fluid=True,
         className="py-3",
