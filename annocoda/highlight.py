b7c80d9274ec6301113d6ad2d07ded1d72b72922
John Moore <jmoore@zedstar.org>
2023-09-25 19:42:29 +0100

    match when punctation in strings

--- annocoda/highlight.py
+++ annocoda/highlight.py
@@ -1,16 +1,17 @@
 class Highlight:
+    
     def __init__(self):
-        pass
+        self.punc_list = '''!()-[]{};*:'"\,<>./?@_~'''
 
     def __markup_worker(self, term, keyword_list):
-        if term.lower() in keyword_list:
+        if term.lower().strip(self.punc_list) in keyword_list:
             return f"**{term}**"
         else:
             return term
 
     def __markup(self, search_value, text):
         search_terms = search_value.split()
-        keywords = map(lambda x: x.lower(), search_terms)
+        keywords = map(lambda x: x.lower().strip(self.punc_list), search_terms)
         keyword_list = list(keywords)
         words = text.split()
         marked_up = map(lambda x: self.__markup_worker(x, keyword_list), words)
